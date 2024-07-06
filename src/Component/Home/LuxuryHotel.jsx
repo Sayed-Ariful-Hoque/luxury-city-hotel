44add48bc3a352d6989047bc13a776336e19f09b
Sayed-Ariful-Sayed <107043600+Sayed-Ariful-Sayed@users.noreply.github.com>
2024-07-06 17:54:31 +0600

    LuxuryHotel Part Responsive Done

--- src/Component/Home/LuxuryHotel.jsx
+++ src/Component/Home/LuxuryHotel.jsx
@@ -7,12 +7,12 @@ function LuxuryHotel() {
     return (
         <>
             <div className='py-40 bg-[#f7f6f6]'>
-                <div className='  w-[1200px] mx-auto flex justify-center items-center gap-6'>
+                <div className='  xl:w-[1200px] mx-auto xL:flex lg:flex justify-center items-center px-3 xl:px-0 lg:px-0 gap-6'>
                     <div className='leftPart'>
                         <div className=" flex justify-center items-center  ">
-                            <div className=' w-[400px] mx-auto'>
+                            <div className=' xl:w-[400px] lg:w-[400px]    mx-auto'>
                                 <TitleLogo className=" font-medium font-serif text-start flex flex-wrap justify-start " pText="welcome" h2Text="" />
-                                <h2 className=' font-medium text-6xl  font-serif' >Luxury Hotel</h2>
+                                <h2 className=' font-medium xl:text-6xl text-5xl  font-serif' >Luxury Hotel</h2>
                                 <h4 className=' font-medium text-3xl font-serif mt-3'>Near The Montmartre, Paris</h4>
                                 <p className=' text-[#473d35] font-semibold font-serif text-[14px] py-3'>Aliquam erat volutpat. Duis ac turpis. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit..</p>
                                 <p className=' text-[#50473f]'>Mauris fermentum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus. </p>
@@ -20,7 +20,7 @@ function LuxuryHotel() {
                             </div>
                         </div>
                     </div>
-                    <div className="rightPart w-[700px]">
+                    <div className="rightPart xl:w-[700px] lg:w-[500px]  ">
                         <img src={LuxuryHotel01} alt="LuxuryHotel01" />
                     </div>
                 </div>
